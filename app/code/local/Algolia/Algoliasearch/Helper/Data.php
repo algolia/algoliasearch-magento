ec400a8525d2b334fb6f38ee60f2d8678cc6d716
Sylvain UTARD <sylvain.utard@gmail.com>
2014-02-18 18:20:53 -0800

    Reindex using batch requests + move code to helper.

--- app/code/local/Algolia/Algoliasearch/Helper/Data.php
+++ app/code/local/Algolia/Algoliasearch/Helper/Data.php
@@ -16,6 +16,46 @@ class Algolia_Algoliasearch_Helper_Data extends Mage_Core_Helper_Abstract {
     return $this->getClient()->initIndex($index)->search($q, $params);
   }
 
+  public function getProductJSON($prod) {
+    $product = Mage::getModel('catalog/product')->load($prod->getId());
+    $categories = array();
+    foreach ($product->getCategoryIds() as $catId) {
+      array_push($categories, Mage::getModel('catalog/category')->load($catId)->getName());
+    }
+    return array(
+      'objectID' => $product->getId(),
+      'name' => $product->getName(),
+      'categories' => $categories,
+      'description' => $product->getDescription(),
+      'price' => $product->getPrice(),
+      'final_price' => $product->getFinalPrice(),
+      'url' => $product->getUrlInStore(),
+      //'thumbnail_url' => $product->getThumbnailUrl(),
+      //'image_url' => $product->getImageUrl()
+    );
+  }
+
+  public function getCategoryJSON($id) {
+    $cat = Mage::getModel('catalog/category'); 
+    $cat->load($id);
+    $path = '';
+    foreach ($cat->getPathIds() as $catId) {
+      if ($path != '') {
+        $path .= ' / ';
+      }
+      $path .= Mage::getModel('catalog/category')->load($catId)->getName();
+    }
+    return array(
+      'objectID' => $id,
+      'name' => $cat->getName(),
+      'path' => $path,
+      'level' => $cat->getLevel(),
+      'url' => $cat->getUrl(),
+      'product_count' => $cat->getProductCount(),
+      //'image_url' => $cat->getImageUrl()
+    );
+  }
+
   private function getClient() {
     return new \AlgoliaSearch\Client($this->getApplicationID(), $this->getAPIKey());
   }
--- app/code/local/Algolia/Algoliasearch/controllers/Adminhtml/AlgoliasearchbackendController.php
+++ app/code/local/Algolia/Algoliasearch/controllers/Adminhtml/AlgoliasearchbackendController.php
@@ -32,26 +32,11 @@ class Algolia_Algoliasearch_Adminhtml_AlgoliasearchbackendController extends Mag
     $tree->load();
     $ids = $tree->getCollection()->getAllIds(); 
     if ($ids) { 
+      $categories = array();
       foreach ($ids as $id) { 
-        $cat = Mage::getModel('catalog/category'); 
-        $cat->load($id);
-        $path = '';
-        foreach ($cat->getPathIds() as $catId) {
-          if ($path != '') {
-            $path .= ' / ';
-          }
-          $path .= Mage::getModel('catalog/category')->load($catId)->getName();
-        }
-        $index_categories->addObject(array(
-          'objectID' => $id,
-          'name' => $cat->getName(),
-          'path' => $path,
-          'level' => $cat->getLevel(),
-          'url' => $cat->getUrl(),
-          'product_count' => $cat->getProductCount(),
-          //'image_url' => $cat->getImageUrl()
-        ));
+        array_push($categories, Mage::helper('algoliasearch')->getCategoryJSON($id));
       } 
+      $index_categories->addObjects($categories);
     }
 
     // products
@@ -59,26 +44,12 @@ class Algolia_Algoliasearch_Adminhtml_AlgoliasearchbackendController extends Mag
     $index_products->setSettings(array(
       "attributesToIndex" => array('name', 'categories', 'unordered(description)')
     ));
-    $products = Mage::getModel('catalog/product')->getCollection();
-    foreach ($products as $prod) {
-      $product = Mage::getModel('catalog/product')->load($prod->getId());
-      $categories = array();
-      foreach ($product->getCategoryIds() as $catId) {
-        array_push($categories, Mage::getModel('catalog/category')->load($catId)->getName());
-      }
-      $json = array(
-        'objectID' => $product->getId(),
-        'name' => $product->getName(),
-        'categories' => $categories,
-        'description' => $product->getDescription(),
-        'price' => $product->getPrice(),
-        'final_price' => $product->getFinalPrice(),
-        'url' => $product->getUrlInStore(),
-        //'thumbnail_url' => $product->getThumbnailUrl(),
-        //'image_url' => $product->getImageUrl()
-      );
-      $index_products->addObject($json);
+    $collection = Mage::getModel('catalog/product')->getCollection();
+    $products = array();
+    foreach ($collection as $prod) {
+      array_push($products, Mage::helper('algoliasearch')->getProductJSON($prod));
     }
+    $index_products->addObjects($products);
 
     $this->_redirect('*/*');
   }
