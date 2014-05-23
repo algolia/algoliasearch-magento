8c3308f03cb27e85a4b06c4d8d2560f51ce958f8
Sergey Kalenyuk <sergey.kalenyuk@gmail.com>
2014-05-23 14:29:13 -0400

    Refactor dashboard template.

--- code/Block/Adminhtml/Algoliasearchbackend.php
+++ code/Block/Adminhtml/Algoliasearchbackend.php
@@ -2,5 +2,30 @@
 
 class Algolia_Algoliasearch_Block_Adminhtml_Algoliasearchbackend extends Mage_Adminhtml_Block_Template
 {
+    protected $_nbEntitiesIndexed = 0;
 
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->init();
+    }
+
+    public function init()
+    {
+        $this->_nbEntitiesIndexed = 0;
+        $indexes = Mage::helper('algoliasearch')->listIndexes();
+        foreach ($indexes['items'] as $index) {
+            $this->_nbEntitiesIndexed += intval($index['entries']);
+        }
+    }
+
+    public function getNbEntitiesIndexed()
+    {
+        return (int) $this->_nbEntitiesIndexed;
+    }
+
+    public function getApplicationId()
+    {
+        return Mage::helper('algoliasearch')->getApplicationID();
+    }
 }
--- code/controllers/Adminhtml/AlgoliasearchbackendController.php
+++ code/controllers/Adminhtml/AlgoliasearchbackendController.php
@@ -6,20 +6,6 @@ class Algolia_Algoliasearch_Adminhtml_AlgoliasearchbackendController extends Mag
     {
         $this->loadLayout();
         $this->_title($this->__('Algolia | Reindex catalog'));
-        $nb_products_indexed = 0;
-        $nb_categories_indexed = 0;
-        $indexes = Mage::helper('algoliasearch')->listIndexes();
-        foreach ($indexes['items'] as $index) {
-            if ($index['name'] == 'magento_products') {
-                $nb_products_indexed = $index['entries'];
-            } else if ($index['name'] == 'magento_categories') {
-                $nb_categories_indexed = $index['entries'];
-            }
-        }
-        $block = $this->getLayout()->getBlock('algoliasearchbackend');
-        $block->setData('nb_products_indexed', $nb_products_indexed);
-        $block->setData('nb_categories_indexed', $nb_categories_indexed);
-        $block->setData('application_id', Mage::helper('algoliasearch')->getApplicationID());
         $this->renderLayout();
     }
 
