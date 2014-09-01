187c61d1d7aad43d9cccbc078cc91fc44580ac6a
Kacper Stasik <kacper.stasik@polcode.com>
2014-09-01 11:45:35 +0200

    Javascript keyboard support

--- skin/livesearch.js
+++ skin/livesearch.js
@@ -8,6 +8,7 @@ AlgoliaLiveSearch.prototype = {
             indexName: null,
             searchDelay: 100,
             minLength: 2,
+            resultLinks: null,
             queryOptions: {
                 hitsPerPage: 10,
                 tagFilters: '(product,category)',
@@ -15,20 +16,27 @@ AlgoliaLiveSearch.prototype = {
                 attributesToHighlight: 'name'
             },
             renderResults: null,
-            clearResults: null
+            clearResults: null,
+            markNext: null,
+            markPrevious: null,
+            selectEntry: null
         }, options || {});
         this.searchForm = new Varien.searchForm('search_mini_form', 'search', this.options.placeholder);
         this.algolia = new AlgoliaSearch(this.options.applicationID, this.options.apiKey);
         this.searchTimeoutId = null;
+        this.active = false;
+        this.index  = 0;
 
         this.submitSearchCallback = this.submitSearch.bind(this);
         this.performSearchCallback = this.performSearch.bind(this);
         this.searchResultsCallback = this.searchResults.bind(this);
+        this.focusOutCallback = this.focusOut.bind(this);
 
         this.searchForm.field
-            .observe('keyup', this.submitSearchCallback)
             .observe('focus', this.submitSearchCallback)
-            .observe('blur', this.options.clearResults ? this.options.clearResults.bind(this) : function(){});
+            .observe('blur', this.options.clearResults ? this.options.clearResults.bind(this) : function(){})
+            .observe('blur', this.focusOutCallback)
+            .observe('keydown', this.onKeyPress.bindAsEventListener(this));
     },
     submitSearch: function(event) {
         if (this.searchTimeoutId) {
@@ -51,6 +59,10 @@ AlgoliaLiveSearch.prototype = {
             Event.fire(document, 'algolia:search', {query: searchQuery});
         }
     },
+    focusOut: function(){
+        this.active = false;
+        this.index = 0;
+    },
     searchResults: function(success, content) {
         if ( ! success) {
             if (console) console.log(content);
@@ -62,6 +74,79 @@ AlgoliaLiveSearch.prototype = {
         ) {
             return;
         }
+
+        this.active = true;
+        this.index = 0;
+        this.selectedEntry = null;
         this.options.renderResults.call(this, content.results[0]);
+    },
+    onKeyPress: function(event) {
+        if(this.active)
+            switch(event.keyCode) {
+                case Event.KEY_TAB:
+                case Event.KEY_RETURN:
+                    this.selectEntry();
+                    Event.stop(event);
+                case Event.KEY_ESC:
+                    this.searchForm.field.blur();
+                    Event.stop(event);
+                    return;
+                case Event.KEY_LEFT:
+                    case Event.KEY_RIGHT:
+                    return;
+                case Event.KEY_UP:
+                    this.markPrevious();
+                    Event.stop(event);
+                    return;
+                case Event.KEY_DOWN:
+                    this.markNext();
+                    Event.stop(event);
+                    return;
+        }
+        else
+            if(event.keyCode==Event.KEY_TAB || event.keyCode==Event.KEY_RETURN ||
+                (Prototype.Browser.WebKit > 0 && event.keyCode == 0)) return;
+        
+        this.submitSearch();
+    },
+    markNext: function() {
+        this.selectedEntry = this.options.markNext ? this.options.markNext.call(this) : this.markEntry(1);
+    },
+    markPrevious: function(){
+        this.selectedEntry = this.options.markPrevious ? this.options.markPrevious.call(this) : this.markEntry(-1);
+    },   
+    selectEntry: function(){
+        if(this.options.selectEntry){
+            this.options.selectEntry.call(this);
+        }
+        else if(this.selectedEntry){
+            this.selectedEntry.click();
+        }
+    },
+    markEntry: function(diff){
+        if(this.options.resultLinks){
+            if(!this.selectedEntry){
+                diff = 0;
+            }
+
+            var links = $$(this.options.resultLinks);
+            if(links.length == 0){
+               this.index = 0;
+               return;
+            }
+               
+            if(this.index+diff >= links.length || this.index+diff < 0){
+                return;
+            }
+
+            this.index += diff;  
+
+            links.invoke('removeClassName', 'marked');
+            links[this.index].addClassName('marked');
+            
+            return links[this.index];
+        }
+
+        return null;
     }
 };
