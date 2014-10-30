4018150a43dc8669b3f4c9a36d2f5ec9bcac3f32
Colin Mollenhour <colin@mollenhour.com>
2014-10-30 15:13:20 -0400

    Skip operations for disabled stores.

--- code/Model/Observer.php
+++ code/Model/Observer.php
@@ -74,6 +74,7 @@ class Algolia_Algoliasearch_Model_Observer
 
         if (is_null($storeId) && is_null($entityId)) {
             foreach (Mage::app()->getStores() as $store) { /** @var $store Mage_Core_Model_Store */
+                if ( ! $store->getIsActive()) { continue; }
                 $this->deleteStoreIndex($store->getId());
             }
         } elseif (is_numeric($storeId) && is_null($entityId)) {
@@ -89,6 +90,7 @@ class Algolia_Algoliasearch_Model_Observer
                 $this->getHelper()->getStoreIndex($storeId)->deleteObjects($objectIds);
             } elseif (is_null($storeId)) {
                 foreach (Mage::app()->getStores() as $store) { /** @var $store Mage_Core_Model_Store */
+                    if ( ! $store->getIsActive()) { continue; }
                     $objectIds = array();
                     foreach ($entityIds as $id) {
                         $objectIds[] = $store->getCode().'_'.$entity.'_'.$id;
