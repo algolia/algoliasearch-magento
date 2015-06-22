8ae0e39be970cd4e02abb7d7d7a44dca18f113cf
Maxime Locqueville <maxiloc@gmail.com>
2015-06-22 23:42:28 +0200

    Bump to 1.2.3 && add release file

--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -23,7 +23,7 @@ class Algolia_Algoliasearch_Helper_Data extends Mage_Core_Helper_Abstract
 
     public function __construct()
     {
-        \AlgoliaSearch\Version::$custom_value = " Magento (1.2.0)";
+        \AlgoliaSearch\Version::$custom_value = " Magento (1.2.3)";
 
         $this->algolia_helper   = Mage::helper('algoliasearch/algoliahelper');
 
