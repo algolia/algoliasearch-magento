36a3a26648856bfe56eab60faec4a9b4bc81d3b6
Maxime Locqueville <maxiloc@gmail.com>
2015-06-29 18:26:52 +0200

    fix url redirection

--- code/Model/Observer.php
+++ code/Model/Observer.php
@@ -204,7 +204,7 @@ class Algolia_Algoliasearch_Model_Observer
 
             $indexName = $this->product_helper->getIndexName(Mage::app()->getStore()->getStoreId());
 
-            $url = Mage::app()->getRequest()->getOriginalPathInfo().'?category=1#q=&page=0&refinements=%5B%7B%22categories%22%3A%22'.$path.'%22%7D%5D&numerics_refinements=%7B%7D&index_name=%22'.$indexName.'%22';
+            $url = Mage::app()->getRequest()->getOriginalPathInfo().'?category=1#q=&page=0&refinements=%5B%7B%22categories%22%3A%5B%22'.$path.'%22%5D%7D%5D&numerics_refinements=%7B%7D&index_name=%22'.$indexName.'%22';
 
             header('Location: '.$url);
 
