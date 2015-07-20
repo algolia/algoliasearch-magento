574e03e7c91a1ff214912b7e824316726dd5be16
vvo <vincent.voyer@algolia.com>
2015-07-20 15:27:30 +0200

    fix: JSON.stringify when using prototype < 1.7
    Fixed in the js client, see algolia/algoliasearch-client-js#123
    
    fixes #148
    fixes #47

--- dev/frontend/scripts/build.sh
+++ dev/frontend/scripts/build.sh
@@ -2,6 +2,6 @@
 set -e # exit when error
 set -x # debug messages
 
-npm update --save
+npm install algoliasearch-extensions-bundle@latest --save
 cp node_modules/algoliasearch-extensions-bundle/bundle.min.js ../../js/bundle.min.js
 cp node_modules/algoliasearch-extensions-bundle/bundle.css ../../skin/bundle.css
