6613c68e44669d79e97672b0f9a3470bc564fd23
vvo <vincent.voyer@algolia.com>
2015-10-01 00:37:43 +0200

    chore: update dev scripts and algoliaBundle to 2.0.0 (autocomplete, instantsearch)

--- dev/frontend/scripts/build.sh
+++ dev/frontend/scripts/build.sh
@@ -3,5 +3,4 @@ set -e # exit when error
 set -x # debug messages
 
 npm install algoliasearch-extensions-bundle@latest --save
-cp node_modules/algoliasearch-extensions-bundle/bundle.min.js ../../js/bundle.min.js
-cp node_modules/algoliasearch-extensions-bundle/bundle.css ../../skin/bundle.css
+cp node_modules/algoliasearch-extensions-bundle/dist/algoliaBundle.min.js ../../js/bundle.min.js
