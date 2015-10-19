2a11dd4dd10bbe657020abaecdf535a95d1af5a3
Maxime Locqueville <maxiloc@gmail.com>
2015-10-19 23:49:12 +0200

    CS

--- code/Helper/Image.php
+++ code/Helper/Image.php
@@ -9,26 +9,23 @@ class Algolia_Algoliasearch_Helper_Image extends Mage_Catalog_Helper_Image
     {
         $model = $this->_getModel();
 
-        if ($this->getImageFile()) {
+        if ($this->getImageFile())
             $model->setBaseFile($this->getImageFile());
-        } else {
+        else
             $model->setBaseFile($this->getProduct()->getData($model->getDestinationSubdir()));
-        }
 
-        if ($model->isCached()) {
+        if ($model->isCached())
             return $model->getUrl();
-        } else {
-            if ($this->_scheduleRotate) {
+        else
+        {
+            if ($this->_scheduleRotate)
                 $model->rotate($this->getAngle());
-            }
 
-            if ($this->_scheduleResize) {
+            if ($this->_scheduleResize)
                 $model->resize();
-            }
 
-            if ($this->getWatermark()) {
+            if ($this->getWatermark())
                 $model->setWatermark($this->getWatermark());
-            }
 
             $url = $model->saveFile()->getUrl();
         }
