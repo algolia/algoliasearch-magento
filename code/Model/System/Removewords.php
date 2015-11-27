a9389722b96848b8d181c2eabd2989345ec6a909
Maxime Locqueville <maxiloc@gmail.com>
2015-11-27 15:04:48 +0100

    fix typo for removewords config

--- code/Model/System/Removewords.php
+++ code/Model/System/Removewords.php
@@ -8,7 +8,7 @@ class Algolia_Algoliasearch_Model_System_Removewords
     public function toOptionArray()
     {
         return array(
-            array('value'=>'None',          'label' => Mage::helper('algoliasearch')->__('None')),
+            array('value'=>'none',          'label' => Mage::helper('algoliasearch')->__('None')),
             array('value'=>'allOptional',   'label' => Mage::helper('algoliasearch')->__('AllOptional')),
             array('value'=>'lastWords',     'label' => Mage::helper('algoliasearch')->__('LastWords')),
             array('value'=>'firstWords',    'label' => Mage::helper('algoliasearch')->__('FirstWords')),
