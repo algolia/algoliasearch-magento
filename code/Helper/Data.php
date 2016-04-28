d2e0138beb221b724bf0ecff8321dae2d5776103
Maxime Locqueville <maxiloc@gmail.com>
2016-04-28 16:03:28 +0200

    bump to 1.5.5

--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -27,7 +27,7 @@ class Algolia_Algoliasearch_Helper_Data extends Mage_Core_Helper_Abstract
 
     public function __construct()
     {
-        \AlgoliaSearch\Version::$custom_value = " Magento (1.5.4)";
+        \AlgoliaSearch\Version::$custom_value = " Magento (1.5.5)";
 
         $this->algolia_helper               = Mage::helper('algoliasearch/algoliahelper');
 
