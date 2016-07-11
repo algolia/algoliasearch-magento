329f9898055933b5d47d6047f5a36986d6226f24
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-07-11 18:08:51 +0200

    Add style to searchbox

--- doc/js/header.js
+++ doc/js/header.js
@@ -1,28 +1,37 @@
 
 $(document).ready(function(){
-    function responsiveNavigation() {
-      var navigation = document.querySelector('.ac-nav');
-      var links = navigation.querySelectorAll('a');
-      var navigationAsSelect = document.createElement('select');
 
-      if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
-        navigationAsSelect.classList.add('display-on-small', 'device');
+  $('#doc-search').on('keyup', function(){
+    if($(this).val()) {
+      $('.searchbox__reset').removeClass('hide')
+    }
+  }).on('blur', function(){
+      $('.searchbox__reset').addClass('hide')
+  });
+
+  function responsiveNavigation() {
+    var navigation = document.querySelector('.ac-nav');
+    var links = navigation.querySelectorAll('a');
+    var navigationAsSelect = document.createElement('select');
+
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
+      navigationAsSelect.classList.add('display-on-small', 'device');
     } else {
-        navigationAsSelect.classList.add('display-on-small');
+      navigationAsSelect.classList.add('display-on-small');
     }
 
     for (var i = 0; i < links.length; i++) {
-        var option = document.createElement('option');
-        option.text = links[i].title;
-        option.value = links[i].href;
-        option.selected = true;
-        navigationAsSelect.appendChild(option);
+      var option = document.createElement('option');
+      option.text = links[i].title;
+      option.value = links[i].href;
+      option.selected = true;
+      navigationAsSelect.appendChild(option);
     }
 
     navigation.appendChild(navigationAsSelect);
     navigation.addEventListener('change', function () {
-        return window.location = e.target.value;
+      return window.location = e.target.value;
     });
-}
-responsiveNavigation();
+  }
+  responsiveNavigation();
 });
