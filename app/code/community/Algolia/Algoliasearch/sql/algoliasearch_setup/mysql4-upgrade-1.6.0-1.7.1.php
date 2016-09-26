6a953e4919574372c4bb437d554f0d85e78ef01c
Jan Petr <honzik.petr@gmail.com>
2016-09-26 10:47:36 +0200

    Change text to longtext

--- app/code/community/Algolia/Algoliasearch/sql/algoliasearch_setup/mysql4-upgrade-1.6.0-1.7.1.php
+++ app/code/community/Algolia/Algoliasearch/sql/algoliasearch_setup/mysql4-upgrade-1.6.0-1.7.1.php
@@ -6,6 +6,6 @@ $installer->startSetup();
 
 $tableName = Mage::getSingleton('core/resource')->getTableName('algoliasearch/queue');
 
-$installer->run('ALTER TABLE ' . $tableName . ' MODIFY data TEXT NOT NULL');
+$installer->run('ALTER TABLE ' . $tableName . ' MODIFY data LONGTEXT NOT NULL');
 
 $installer->endSetup();
