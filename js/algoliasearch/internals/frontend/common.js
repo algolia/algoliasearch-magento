d132185b2ae7cf5584e0c3ac4659480d98f5719a
Jan Petr <honzik.petr@gmail.com>
2017-12-21 11:13:56 +0100

    Fix expired special prices

--- js/algoliasearch/internals/frontend/common.js
+++ js/algoliasearch/internals/frontend/common.js
@@ -95,6 +95,17 @@ document.addEventListener("DOMContentLoaded", function (e) {
 			if (hit['price'] !== undefined && price_key !== '.' + algoliaConfig.currencyCode + '.default' && hit['price'][algoliaConfig.currencyCode][price_key.substr(1) + '_formated'] !== hit['price'][algoliaConfig.currencyCode]['default_formated']) {
 				hit['price'][algoliaConfig.currencyCode][price_key.substr(1) + '_original_formated'] = hit['price'][algoliaConfig.currencyCode]['default_formated'];
 			}
+			
+			if (hit['price'][algoliaConfig.currencyCode]['default_original_formated']
+				&& hit['price'][algoliaConfig.currencyCode]['special_to_date']) {
+				var priceExpiration = hit['price'][algoliaConfig.currencyCode]['special_to_date'];
+
+				if (algoliaConfig.now > priceExpiration) {
+					console.log('here');
+					hit['price'][algoliaConfig.currencyCode]['default_formated'] = hit['price'][algoliaConfig.currencyCode]['default_original_formated'];
+					hit['price'][algoliaConfig.currencyCode]['default_original_formated'] = false;
+				}
+			}
 
 			return hit;
 		};
