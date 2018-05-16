caa36a1c1717587b9549ea76fe3489817eb7dfaf
Damien Couchez <damien.couchez@gmail.com>
2018-05-16 13:59:51 +0200

    Fix config and js

--- js/algoliasearch/click_conversion_analytics.js
+++ js/algoliasearch/click_conversion_analytics.js
@@ -1,45 +1,44 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	algoliaBundle.$(function ($) {
-		AlgoliaAnalytics.init({
-			applicationID: algoliaConfig.applicationId,
-			apiKey: algoliaConfig.instant.apiKey
-		});
+algoliaBundle.$(function ($) {
+	AlgoliaAnalytics.init({
+		applicationID: algoliaConfig.applicationId,
+		apiKey: algoliaConfig.instant.apiKey
+	});
 
-		$(algoliaConfig.autocomplete.selector).each(function () {
-			$(this).on('autocomplete:selected', function (e, suggestion) {
-				AlgoliaAnalytics.click({
-					queryID: suggestion.__queryID,
-					objectID: suggestion.objectID,
-					position: suggestion.__position
-				});
-			});
-		});
-
-		$(document).on('click', algoliaConfig.ccAnalytics.ISSelector, function() {
-			var $this = $(this);
-
+	$(algoliaConfig.autocomplete.selector).each(function () {
+		$(this).on('autocomplete:selected', function (e, suggestion) {
 			AlgoliaAnalytics.click({
-				objectID: $this.data('objectid').toString(),
-				position: parseInt($this.data('position'))
+				queryID: suggestion.__queryID,
+				objectID: suggestion.objectID,
+				position: suggestion.__position
 			});
 		});
 	});
 
-	algolia.registerHook('beforeInstantsearchInit', function (instantsearchOptions) {
-		instantsearchOptions.searchParameters['clickAnalytics'] = true;
-
-		return instantsearchOptions;
-	});
-
-	algolia.registerHook('beforeInstantsearchStart', function (search) {
-		search.once('render', function() {
-			AlgoliaAnalytics.initSearch({
-				getQueryID: function() {
-					return search.helper.lastResults && search.helper.lastResults._rawResults[0].queryID
-				}
-			});
+	$(document).on('click', algoliaConfig.ccAnalytics.ISSelector, function() {
+		var $this = $(this);
+		AlgoliaAnalytics.click({
+			objectID: $this.data('objectid').toString(),
+			position: parseInt($this.data('position'))
 		});
-
-		return search;
 	});
+});
+
+algolia.registerHook('beforeInstantsearchInit', function (instantsearchOptions) {
+	instantsearchOptions.searchParameters['clickAnalytics'] = true;
+
+	return instantsearchOptions;
+});
+
+algolia.registerHook('beforeInstantsearchStart', function (search) {
+
+	search.once('render', function() {
+		AlgoliaAnalytics.initSearch({
+			getQueryID: function() {
+				console.log(search);
+				return search.helper.lastResults && search.helper.lastResults._rawResults[0].queryID
+			}
+		});
+	});
+
+	return search;
 });
