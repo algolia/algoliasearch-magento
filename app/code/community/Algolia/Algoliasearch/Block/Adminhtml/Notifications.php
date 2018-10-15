9ff3911891b5c8a56bd7d83b72f29804e35806e8
Betty Suravech <bsuravech@gmail.com>
2018-10-15 10:36:41 -0700

    update getQueueInfo method for readable structure

--- app/code/community/Algolia/Algoliasearch/Block/Adminhtml/Notifications.php
+++ app/code/community/Algolia/Algoliasearch/Block/Adminhtml/Notifications.php
@@ -19,37 +19,39 @@ class Algolia_Algoliasearch_Block_Adminhtml_Notifications extends Mage_Adminhtml
 
     public function getQueueInfo()
     {
-        if (!$this->_queueInfo) {
-            /** @var Algolia_Algoliasearch_Helper_Config $config */
-            $config = Mage::helper('algoliasearch/config');
-
-            /** @var Mage_Core_Model_Resource $resource */
-            $resource = Mage::getSingleton('core/resource');
-            $tableName = $resource->getTableName('algoliasearch/queue');
-
-            $readConnection = $resource->getConnection('core_read');
-
-            $size = (int)$readConnection->query('SELECT COUNT(*) as total_count FROM ' . $tableName)->fetchColumn(0);
-            $maxJobsPerSingleRun = $config->getNumberOfJobToRun();
-
-            $etaMinutes = ceil($size / $maxJobsPerSingleRun) * 5; // 5 - assuming the queue runner runs every 5 minutes
-
-            $eta = $etaMinutes . ' minutes';
-            if ($etaMinutes > 60) {
-                $hours = floor($etaMinutes / 60);
-                $restMinutes = $etaMinutes % 60;
-
-                $eta = $hours . ' hours ' . $restMinutes . ' minutes';
-            }
-
-            $queueInfo = array(
-                'isEnabled' => $config->isQueueActive(),
-                'currentSize' => $size,
-                'eta' => $eta,
-            );
-
-            $this->_queueInfo = $queueInfo;
+        if (isset($this->_queueInfo)) {
+            return $this->_queueInfo;
         }
+
+        /** @var Algolia_Algoliasearch_Helper_Config $config */
+        $config = Mage::helper('algoliasearch/config');
+
+        /** @var Mage_Core_Model_Resource $resource */
+        $resource = Mage::getSingleton('core/resource');
+        $tableName = $resource->getTableName('algoliasearch/queue');
+
+        $readConnection = $resource->getConnection('core_read');
+
+        $size = (int)$readConnection->query('SELECT COUNT(*) as total_count FROM ' . $tableName)->fetchColumn(0);
+        $maxJobsPerSingleRun = $config->getNumberOfJobToRun();
+
+        $etaMinutes = ceil($size / $maxJobsPerSingleRun) * 5; // 5 - assuming the queue runner runs every 5 minutes
+
+        $eta = $etaMinutes . ' minutes';
+        if ($etaMinutes > 60) {
+            $hours = floor($etaMinutes / 60);
+            $restMinutes = $etaMinutes % 60;
+
+            $eta = $hours . ' hours ' . $restMinutes . ' minutes';
+        }
+
+        $queueInfo = array(
+            'isEnabled' => $config->isQueueActive(),
+            'currentSize' => $size,
+            'eta' => $eta,
+        );
+
+        $this->_queueInfo = $queueInfo;
 
         return $this->_queueInfo;
     }
